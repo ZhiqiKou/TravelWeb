08622aeb5efa00c1ce352e8d856ec26fedcf4136
zhiqi_ubuntu <935939006@qq.com>
2018-10-15 10:47:04 +0800

    新闻资讯功能完善

--- zhiqiTravel/apps/news/views.py
+++ zhiqiTravel/apps/news/views.py
@@ -10,6 +10,10 @@ class NewsView(View):
     def get(self, request):
 
         all_news = News.objects.all().order_by('-add_times')
+        culture = all_news.filter(classification='culture')[:3]
+        specialty = all_news.filter(classification='specialty')[:3]
+        food = all_news.filter(classification='food')[:3]
+        life = all_news.filter(classification='life')[:3]
 
         news_type = request.GET.get('classification', '')
         if news_type:
@@ -18,6 +22,10 @@ class NewsView(View):
         return render(request, 'news_list.html', {
             'all_news': all_news,
             'classification': news_type,
+            'culture': culture,
+            'specialty': specialty,
+            'food': food,
+            'life': life,
         })
 
 
@@ -26,10 +34,20 @@ class NewsDetails(View):
     新闻详情页
     """
     def get(self, request, news_id):
+        all_news = News.objects.all().order_by('-add_times')
+        culture = all_news.filter(classification='culture')[:3]
+        specialty = all_news.filter(classification='specialty')[:3]
+        food = all_news.filter(classification='food')[:3]
+        life = all_news.filter(classification='life')[:3]
+
         news = News.objects.get(id=(int(news_id)))
         news.checknum += 1
         news.save()
 
         return render(request, 'article.html', {
             'news': news,
+            'culture': culture,
+            'specialty': specialty,
+            'food': food,
+            'life': life,
         })
